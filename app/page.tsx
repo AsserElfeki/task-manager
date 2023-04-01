0ec3880a95cf7bf8ba2be797a5718f19e1579eb2
Asser <66274223+AsserElfeki@users.noreply.github.com>
2023-04-01 18:25:36 +0200

    setting up

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import { Inter } from '@next/font/google'
 import styles from './page.module.css'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -13,6 +13,7 @@ export default function Home() {
           <code className={styles.code}>app/page.tsx</code>
         </p>
         <div>
+
           <a
             href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
             target="_blank"
@@ -89,3 +90,4 @@ export default function Home() {
     </main>
   )
 }
+
