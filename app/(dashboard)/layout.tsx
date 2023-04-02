43a7c33378ea5dad37b19b929c5bffcca3137cc9
Asser <66274223+AsserElfeki@users.noreply.github.com>
2023-04-02 16:00:58 +0200

    pages boiler plate

---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,26 @@
+import GlassPane from '@/components/GlassPane';
+import "@/styles/global.css";
+// import { Inter } from "@next/font/google";
+
+// const inter = Inter({
+//   variable: "--font-inter",
+// });
+
+export default function DashboardRootLayout({ children }) {
+  return (
+    <html lang="en" >
+          <head>
+              {/* <meta charSet="utf-8" /> */}
+              <meta name="viewport" content="width=device-width, initial-scale=1" />
+              <link rel="icon" href="/favicon.ico" />
+              <title>Next.js + Tailwind CSS</title>
+          </head>
+          
+      <body className="h-screen w-screen rainbow-mesh p-6">
+        <GlassPane className="w-full h-full flex items-center justify-center">
+          {children}
+        </GlassPane>
+      </body>
+    </html>
+  );
+}
