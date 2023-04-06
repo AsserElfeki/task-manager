e62b62f60434c89f2c88f07a5191f0de2da32bcb
Asser <66274223+AsserElfeki@users.noreply.github.com>
2023-04-06 16:00:19 +0200

    greetings and directory fixes

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,28 +1,18 @@
-import GlassPane from '@/components/GlassPane';
-import "@/styles/global.css";
-import { Sidebar } from 'react-feather';
-import { Inter } from "@next/font/google";
+import '@/styles/global.css'
+import GlassPane from "@/components/GlassPane";
+import Sidebar from '@/components/Sidebar';
 
-const inter = Inter({
-  variable: "--font-inter",
-});
-
-export default function DashboardRootLayout({ children }) {
+export default function DashboardRootLayout({children}) {
   return (
-    <html lang="en" >
-          <head>
-              {/* <meta charSet="utf-8" /> */}
-              <meta name="viewport" content="width=device-width, initial-scale=1" />
-              <link rel="icon" href="/favicon.ico" />
-              <title>Next.js + Tailwind CSS</title>
-          </head>
-          
+    <html lang="en">
+      <head />
       <body className="h-screen w-screen candy-mesh p-6">
-        <GlassPane className="w-full h-full flex items-center justify-center">
-          <Sidebar/>
+        <GlassPane
+          className="w-full h-full flex items-center">
+          <Sidebar />
           {children}
         </GlassPane>
       </body>
     </html>
-  );
+  )
 }
